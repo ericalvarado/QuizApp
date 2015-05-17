1f2a1ffce63525dbe795c761ef87275c982fb32f
ericalvarado <ericalvarado@gmail.com>
2015-05-17 10:34:58 -0400

    Completed Lesson 47 - Adding Question and Answer Text to the View.

---
+++ QuizApp/AnswerButtonView.swift
@@ -0,0 +1,46 @@
+//
+//  AnswerButton.swift
+//  QuizApp
+//
+//  Created by Eric Alvarado on 5/16/15.
+//  Copyright (c) 2015 Eric Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class AnswerButtonView: UIView {
+    
+    let answerLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        //Add the label to the view
+        self.addSubview(answerLabel)
+        answerLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setAnswerText(answer:String){
+        answerLabel.text = answer
+        
+        //Set properties for the label
+        answerLabel.numberOfLines = 0
+        answerLabel.textColor = UIColor.whiteColor()
+        answerLabel.textAlignment = NSTextAlignment.Center
+        answerLabel.adjustsFontSizeToFitWidth = true
+        answerLabel.font = UIFont(name: "HelveticaNeue-Thin", size: 18)
+        
+        //Set constraints
+        let leftMarginConstraint = NSLayoutConstraint(item: answerLabel, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 20)
+        let rightMarginConstraint = NSLayoutConstraint(item: answerLabel, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: -20)
+        let topMarginConstraint = NSLayoutConstraint(item: answerLabel, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 5)
+        let bottomMarginConstraint = NSLayoutConstraint(item: answerLabel, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: -5)
+        
+        self.addConstraints([leftMarginConstraint,rightMarginConstraint,topMarginConstraint,bottomMarginConstraint])
+        
+    }
+}
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -9,26 +9,89 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let quizModel = QuizModel()
-    var questions = [Question]()
-    
+   
     @IBOutlet weak var questionView: UIView!
     @IBOutlet weak var answerViewContentView: UIView!
     @IBOutlet weak var questionText: UILabel!
     @IBOutlet weak var moduleLessonText: UILabel!
-    
+
+    let quizModel = QuizModel()
+    var questions = [Question]()
+    var currentQuestion = Question?()
+    var answerButtonAnswerArray = [AnswerButtonView]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Get the questions from the quiz model
         questions = quizModel.getQuestions()
+        
+        // Check to see if there is at least one question
+        if (questions.count > 0){
+            // Set the current question to the first question
+            currentQuestion = questions[0]
+            
+            // Call the display question method
+            displayCurrentQuestion()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func displayCurrentQuestion(){
+        //Confirm that there is question text
+        
+        if currentQuestion?.questionText != nil {
+
+            // Update the question text
+            questionText.text = currentQuestion?.questionText
+            
+            // Update the module and lesson text
+            moduleLessonText.text = "Module \(currentQuestion!.module) | Lesson \(currentQuestion!.lesson)"
+            
+            // Create and display the answer button views
+            createAnswerButton()
+        }
+    }
+    
+    func createAnswerButton(){
+        
+        for var index=0; index<currentQuestion!.answers.count;index++ {
+
+            // Create an answer button view
+            let answer = AnswerButtonView()
+            
+            // Turn off Autoresizing Mask
+            answer.setTranslatesAutoresizingMaskIntoConstraints(false)
+            
+            // Place it into the content view
+            answerViewContentView.addSubview(answer)
+            
+            // Add constraints to the button depending on which one it is
+            var heightConstraint = NSLayoutConstraint(item: answer, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 100)
+            answer.addConstraint(heightConstraint)
+            
+            var leftConstraint = NSLayoutConstraint(item: answer, attribute: NSLayoutAttribute.Left, relatedBy: NSLayoutRelation.Equal, toItem: answerViewContentView, attribute: NSLayoutAttribute.Left, multiplier: 1, constant: 0)
+            var rightConstraint = NSLayoutConstraint(item: answer, attribute: NSLayoutAttribute.Right, relatedBy: NSLayoutRelation.Equal, toItem: answerViewContentView, attribute: NSLayoutAttribute.Right, multiplier: 1, constant: 0)
+            var topConstraint = NSLayoutConstraint(item: answer, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: answerViewContentView, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: CGFloat(100*index))
+            
+            answerViewContentView.addConstraints([leftConstraint,rightConstraint,topConstraint])
+            
+            // Set the answer text for it
+            answer.setAnswerText(currentQuestion!.answers[index])
+            
+            // Add it to the button array
+            answerButtonAnswerArray.append(answer)
+            
+        }
+        
+        // Adjust the height of the scroll view if needed
+        var scrollHeightConstraint = NSLayoutConstraint(item: answerViewContentView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: CGFloat(101 * answerButtonAnswerArray.count))
+
+        answerViewContentView.addConstraint(scrollHeightConstraint)
     }
 
 }
